abab4da2f246cf20b4c524f695d9afadba09bcec
developiaa <developiaa@gmail.com>
2022-03-02 23:15:26 +0900

    first commit

---
+++ converter/src/test/java/blog/developia/converter/ConverterApplicationTests.java
@@ -0,0 +1,13 @@
+package blog.developia.converter;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConverterApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
